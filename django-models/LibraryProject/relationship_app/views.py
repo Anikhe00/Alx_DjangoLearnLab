d3df23b18adb7721f72b825f885cd495dba028b3
Shakirat Akanji <akanjishakirato@gmail.com>
2025-11-09 22:30:22 +0100

    Task 1 completed

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from .models import Book, Library
+from django.views.generic import DetailView
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    
+    output = ""
+    for book in books:
+        output += f"{book.title} by {book.author.name}\n"
+
+    if not output:
+        output = "No books available."
+    
+    return HttpResponse(output, content_type="text/plain")
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
